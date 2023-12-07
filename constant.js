e87253ab4023058a5c41cdffdb4fd2b0efd4adca
Sviatoslav O <s.terletskyi@ocado.com>
2023-12-07 15:41:40 +0200

    fixed assistant creation

--- constant.js
+++ constant.js
@@ -1,19 +1,36 @@
-const MODEL_NAME = 'gpt-4-1106-preview';
-// const MODEL_NAME = 'gpt-3.5-turbo-16k';
-const ASSISTANT_NAME = 'Agent RealtyGPT';
-const ASSISTANT_DESCRIPTION = 'Script-following chatbot for real estate lead qualification.';
-const ASSISTANT_PROMPT = `You are ${ASSISTANT_NAME}, a friendly and engaging chatbot designed for real estate agents to assist in lead qualification. Your primary role is to follow a specific user-uploaded script to gather necessary information in a conversational manner. This script guides you through a series of questions to determine if a lead is interested in buying, renting, or selling a property, and includes various scenarios based on the lead's responses. You should ask one question at a time, following the script's flow, ensuring the conversation is natural and easy for users to follow. If a lead provides incomplete answers, politely rephrase and ask again until you receive a satisfactory response. Do not offer advice; prompt users to consult a real estate agent for more information. Your communication style is engaging, ensuring a warm and friendly interaction while being concise and to the point. For better user experience tell how many questions are remaining to ask. For example, I have 10 questions to ask. For every question you ask provide how many questions remaining (5 out of 10).
-
-Before Thank You message, capture the lead info. 
-`;
-
 const FUNCTION_NAMES = {
   captureBuyLead: 'capture_buy_lead',
   captureSellLead: 'capture_sell_lead',
   captureRentLead: 'capture_rent_lead',
+  captureLeadNurture: 'capture_lead_nurture',
 };
 
+const MODEL_NAME = 'gpt-4-1106-preview';
+// const MODEL_NAME = 'gpt-3.5-turbo-16k';
+const ASSISTANT_NAME = 'Agent RealtyGPT';
+const ASSISTANT_DESCRIPTION = 'Script-following chatbot for real estate lead qualification.';
+const ASSISTANT_PROMPT = `You are ${ASSISTANT_NAME}, a friendly and engaging chatbot designed for real estate agents to assist in lead qualification. Your primary role is to follow a specific user-uploaded script to gather necessary information in a conversational manner. This script guides you through a series of questions to determine if a lead is interested in buying, renting, or selling a property, and includes various scenarios based on the lead's responses. You should ask one question at a time, following the script's flow, ensuring the conversation is natural and easy for users to follow. If a lead provides incomplete answers, politely rephrase and ask again until you receive a satisfactory response. Do not offer advice; prompt users to consult a real estate agent for more information. Your communication style is engaging, ensuring a warm and friendly interaction while being concise and to the point. 
+
+For better user experience tell how many questions are remaining to ask.
+
+For example, Depends on your preferences I have between 10 and 15 questions to ask. For every question you ask provide how many questions you have asked (use following format 1st or 2nd, 3rd, 4th etc).
+
+Before Thank You message, capture the lead info. 
+
+Today is Dec 2023. Use it for date calculations if necessary.
+
+If lead wants to buy/sell/rent in more than 6 months follow ${FUNCTION_NAMES.captureLeadNurture} function. ask all necessary questions don't assume answers here.
+
+If someone give answers in date use code_interpreter to get todays date and use this date for calculation. 
+For instance, 
+Q: when you want to buy?
+A: on jan 2024
+`
+
 const TOOLS = [
+  {
+    type: 'code_interpreter',
+  },
   {
     type: "function",
     function: {
@@ -24,7 +41,7 @@ const TOOLS = [
         properties: {
           whenReady: {
             type: 'string',
-            description: "How soon is lead ready to buy a property?. If more than 6 months, say good bye."
+            description: "How soon is lead ready to buy a property?"
           },
           workWithAgent: {
             type: 'string',
@@ -110,15 +127,13 @@ const TOOLS = [
 
         },
         required: [
-          "name", 
-          "email", 
-          "phoneNumber", 
-          "whenReady", 
-          "workWithAgent", 
-          "rentOrOwn", 
-          "planningToSell",
+          "name",
+          "email",
+          "phoneNumber",
+          "whenReady",
+          "workWithAgent",
+          "rentOrOwn",
           "firstTimeHomeBuyer",
-          "freeHomeEvaluation",
           "homeTitle",
           "priceRange",
           "size",
@@ -142,7 +157,7 @@ const TOOLS = [
         properties: {
           whenReady: {
             type: 'string',
-            description: "How soon is lead ready to sell a property?. If more than 6 months, say good bye."
+            description: "How soon is lead ready to sell a property?"
           },
           workWithAgent: {
             type: 'string',
@@ -203,11 +218,11 @@ const TOOLS = [
 
         },
         required: [
-          "name", 
-          "email", 
-          "phoneNumber", 
-          "whenReady", 
-          "workWithAgent", 
+          "name",
+          "email",
+          "phoneNumber",
+          "whenReady",
+          "workWithAgent",
           "reason",
           "bedrooms",
           "bathrooms",
@@ -230,7 +245,7 @@ const TOOLS = [
         properties: {
           whenReady: {
             type: 'string',
-            description: "How soon is lead ready to rent a property?. If more than 6 months, say good bye."
+            description: "How soon is lead ready to rent a property?"
           },
           workWithAgent: {
             type: 'string',
@@ -292,14 +307,18 @@ const TOOLS = [
             type: 'string',
             description: "The phone number of lead you are speaking to"
           },
+          bestTimeToContact: {
+            type: 'string',
+            description: '',
+          },
 
         },
         required: [
-          "name", 
-          "email", 
-          "phoneNumber", 
-          "whenReady", 
-          "workWithAgent", 
+          "name",
+          "email",
+          "phoneNumber",
+          "whenReady",
+          "workWithAgent",
           "budget",
           "creditScore",
           "send410Form",
@@ -309,6 +328,42 @@ const TOOLS = [
           "locationPreferences",
           "amenities",
           "leaseDuration",
+        ],
+      },
+    },
+  },
+  {
+    type: "function",
+    function: {
+      name: 'capture_lead_nurture',
+      description: "Capture leads that want to buy or rent or sell in more than 6 months.",
+      parameters: {
+        type: "object",
+        properties: {
+          actionType: {
+            type: "string",
+            description: "Whether lead want to sell/rent/buy. Don't ask the question. Assume it",
+            enum: ['Buy', 'Rent', 'Sell'],
+          },
+          name: {
+            type: "string",
+            description: "The full name of lead you are speaking to"
+          },
+          email: {
+            type: "string",
+            description: "The email of lead you are speaking to"
+          },
+          phoneNumber: {
+            type: 'string',
+            description: "The phone number of lead you are speaking to"
+          },
+
+        },
+        required: [
+          "actionType",
+          "name",
+          "email",
+          "phoneNumber",
         ],
       },
     },
